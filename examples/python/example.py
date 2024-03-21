3200174597a7ceee15460404a82b8f29999c5bf6
sofiabadilla <sofy.bobadilla@gmail.com>
2024-03-21 14:44:24 +0100

    fix: now example.py reads from the new vuns-x.json files

--- examples/python/example.py
+++ examples/python/example.py
@@ -5,6 +5,8 @@ class VulnDb:
         self.issues = [json.loads(l) for l in data.split('\n') if l.strip()]
 
 if __name__ == '__main__':
-    with open('../../dataset/vulns.json', 'r') as f:
-        db = VulnDb(f.read())
-    print("total issues: %d"%len(db.issues))
+    with open('../../dataset/vulns-1.json', 'r', encoding="utf-8") as f:
+        db1 = VulnDb(f.read())
+    with open('../../dataset/vulns-2.json', 'r', encoding="utf-8") as f:
+        db2 = VulnDb(f.read())
+    print("total issues: %d"%(len(db1.issues)+len(db2.issues)))
